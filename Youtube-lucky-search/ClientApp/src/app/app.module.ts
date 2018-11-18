13e522f82d25af68c4b3b00f8c970147af230659
Vladislav Cotlearov <vladislav.kotlyarov@outlook.com>
2018-11-18 18:13:27 +0200

    * Adjusted Material and CDK to version 5.2.4;
    * Implemented Angular UI and parameters for the Search;

--- Youtube-lucky-search/ClientApp/src/app/app.module.ts
+++ Youtube-lucky-search/ClientApp/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
-//import { MatButtonModule, MatCheckboxModule } from '@angular/material';
-//import { MatInputModule } from '@angular/material';
-//import { MatSelectModule } from '@angular/material';
-//import { MatRadioModule } from '@angular/material';
+import { MatInputModule } from '@angular/material';
+import { MatSelectModule } from '@angular/material';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -27,14 +28,18 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
   ],
   imports: [
       BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-      //MatButtonModule, MatCheckboxModule, MatInputModule, MatSelectModule,MatRadioModule,
+      BrowserAnimationsModule,
+      MatButtonModule,
+      MatInputModule,
+      MatSelectModule,
+      MatRadioModule,
     HttpClientModule,
       FormsModule, 
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-    ])
+      ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Youtube-lucky-search/ClientApp/src/app/home/home.component.ts
+++ Youtube-lucky-search/ClientApp/src/app/home/home.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { DomSanitizer, SafeUrl, SafeResourceUrl } from "@angular/platform-browser";
+
+export interface VideoOption {
+    value: string;
+    viewValue: string;
+}
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
-    favoriteSeason: string;
-    seasons: string[] = ['Winter', 'Spring', 'Summer', 'Autumn'];
+export class HomeComponent implements OnInit {
+    ngOnInit(): void {
+        
+    }
+    chosenKeyword: string;
+
+    chosenLength: string;
+    lengths: VideoOption[] = [
+        { value: 'Short', viewValue: 'Short (<4 mins)' },
+        { value: 'Medium', viewValue: 'Medium (4-20 mins)' },
+        { value: 'Long', viewValue: 'Long (>20 mins)' }
+    ];
+
+    chosenPopularity: string;
+    popularities: VideoOption[] = [
+        { value: 'Private', viewValue: 'Private' },
+        { value: 'Niche', viewValue: 'Niche (>5k views)' },
+        { value: 'Popular', viewValue: 'Popular (>50k views)' },
+        { value: 'Viral', viewValue: 'Viral (>500k views)' },
+        { value: 'OMG', viewValue: 'OMG (>1M views)' }
+    ];
+
+    chosenRating: string;
+    ratings: VideoOption[] = [
+        { value: 'Disaster', viewValue: 'Disaster (<40%)' },
+        { value: 'Soso', viewValue: 'So-so (40-70%)' },
+        { value: 'Good', viewValue: 'Good (70-90%)' },
+        { value: 'Awesome', viewValue: 'Awesome (>90%)' }
+    ];
+
+
+    videoLoaded = true;
+    videoTemplate = "https://www.youtube.com/embed/";
+    completeURL: SafeResourceUrl;
+    videoID: string;
+
+    constructor(private sanitizer: DomSanitizer)
+    {
+        this.videoLoaded = false;
+    }
+
+    public GetVideo()
+    {
+        this.videoID = "YE7VzlLtp-4";
+        this.completeURL = this.sanitizer.bypassSecurityTrustResourceUrl(this.videoTemplate + this.videoID);
+
+        this.videoLoaded = !this.videoLoaded;
+    }
 }
